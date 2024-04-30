880bdd91a4fa2e7310083d72104fe9bf5bb65451
Loai Hammoud <112626695+LoayHamod@users.noreply.github.com>
2024-04-30 13:49:33 +0300

    Update notice.model.js

--- models/notice.model.js
+++ models/notice.model.js
@@ -13,10 +13,10 @@ const noticeSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin'
-    },
+    // school: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'admin'
+    // },
 }, { timestamps: true });
 
 module.exports = mongoose.model("notice", noticeSchema)
